7d546e9360eb70fd8ff550dc8bb8ba2ff3c75229
Andrew Ahn <andrewahn21@gmail.com>
2024-12-18 22:51:38 -0800

    Wrote function signature for forward propagation

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # Feed-Forward Neural Network
-A simple neural network built from scratch (ie. using only numpy, no tensorflow or pytorch) as an exercise to learn machine learning. 
+
+A simple neural network built from scratch (ie. using only numpy, no tensorflow or pytorch) as an exercise to learn machine learning.
 
 ## Background
+
 I'm a complete ML noob. There are a lot of tutorials on how to build an NN from scratch but I want to figure it out on my own so I'll probably stumble around make dumb mistakes. I watched [3B1B's series on neural networks](https://youtu.be/aircAruvnKk?si=3YaX6TYLx1CXsgmj) to get a theoretical understanding of NNs, but other than that, I'm going in blind. That's my goat btw
 
 > "What I cannot create, I do not understand" -Richard Feynman
 
-
 ## Implementation Details
+
 I'll flesh out this section with what design choices I made once I've finished building it lol
 
 ## Some goals
+
 - Once the implementation is done, test it on the MNIST hand-written digits dataset and get a relatively high accuracy (anything >80%).
 - Explore some fun deep learning techniques (learning rate schedules, early stopping, different gradient descent variations, hyperparameter tuning, different initializtion methods, etc.).
--- neural_network.py
+++ neural_network.py
@@ -21,10 +21,20 @@ class NeuralNetwork():
         self.secondBiasVector = np.random.random(16)
         self.thirdBiasVector = np.random.random(10)
 
+    # Function: Propagates the input values "forward" through the neural network and changes the 
+    #           activations in the hidden layers and the output layer.
+    # Input: A single image - an array of size 784 containing doubles, and
+    #        the target number that is drawn in the image.
+    # Output: None 
+    # Usage: To be used before performing backpropagation on a single image. 
+    #        Will be called once per image for each gradient descent step.
+    def forward(self, image):
+        pass
+
     # Function: The "heart" of the backpropagation algorithm, performed on a single training example
-    # Input: a single image - an array of size 784 containing doubles, and
-    #        the target number that is drawn in the image
-    # Output: the gradient vector for the single image - as an array of size 13,002 containing doubles
+    # Input: A single image - an array of size 784 containing doubles, and
+    #        the target number that is drawn in the image.
+    # Output: The gradient vector for the single image - as an array of size 13,002 containing doubles.
     # Usage: To be called on every single training example by the main backpropagation function.
     def backpropOneImage(self, image, target):
         pass
@@ -32,9 +42,9 @@ class NeuralNetwork():
     # Function: Facilitates backpropagation by calling the backpropOneImage function on 
     #           each training example, then averaging out and scaling the resulting gradient vectors
     #           to find the next "step" in the gradient descent.
-    # Input: a python dictionary where the keys are the training images - arrays of size 784 containing doubles, and
-    #        the values are the target numbers that are drawn in the corresponding training image
-    # Output: the gradient vector for the given set of images  - as an array of size 13,002 containing doubles
+    # Input: A python dictionary where the keys are the training images - arrays of size 784 containing doubles, and
+    #        the values are the target numbers that are drawn in the corresponding training image.
+    # Output: The gradient vector for the given set of images  - as an array of size 13,002 containing doubles.
     # Usage: To be called on a "batch" of training images to find the next gradient descent step.
     def backpropagation(self, trainingData):
         # gradientVectors is a 2D array, where each row will be a single gradient vector 
@@ -54,9 +64,9 @@ class NeuralNetwork():
         
     # Function: Helper function to take in the output of backpropagation and convert it 
     #           to a form that is easier to work with when updating the parameters
-    # Input: a gradient vector - as an array of size 13,002 containing doubles
-    # Output: a tuple containing the components of the gradient vector organized into 
-    #         the appropriate matrices and vectors
+    # Input: A gradient vector - as an array of size 13,002 containing doubles.
+    # Output: A tuple containing the components of the gradient vector organized into 
+    #         the appropriate matrices and vectors.
     # Usage: To be used after performing backprop on a set of training examples and having found the 
     #        average gradient vector. Would be called once for each gradient descent step.
     def paramVectorToTuple(self, gradientVector):
@@ -65,22 +75,22 @@ class NeuralNetwork():
     # Function: Helper function to apply the gradient descent step to the neural network by 
     #           adding the matrices and vectors in the tuple to the neural network 
     #           instance's matrices and vectors.
-    # Input: a tuple containing the components of the gradient vector organized into the appropriate
-    #        matrices and vectors
-    # Output: none
+    # Input: A tuple containing the components of the gradient vector organized into the appropriate
+    #        matrices and vectors.
+    # Output: None
     # Usage: To be used after calling the paramVectorToTuple function. Would be called once
     #        for each gradient descent step.
     def applyGradientDescentStep(self, tuple):
         pass
 
     # Function: Perform stochastic gradient descent (SGD) by running backpropagation on training data 
-    #           batches repeatedly until the resulting gradient vector is sufficiently small 
+    #           batches repeatedly until the resulting gradient vector is sufficiently small.
     #           (or some other condition has been satisfied for us to stop gradient descent - TBD).
-    # Input: all of the training data - format TBD
-    # Output: none
+    # Input: All of the training data - format TBD.
+    # Output: None
     # Usage: First point of contact for training the neural network. Would only be called once for each
     #        time someone wants to train the NN. Manages training data batching, iterative backpropagation,
-    #        and the stopping condition
+    #        and the stopping condition.
     def trainNeuralNetwork(self):
         pass
 
